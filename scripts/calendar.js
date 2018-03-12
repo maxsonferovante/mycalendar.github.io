d3c6908477a847835dc8a1aaccebd8be6165712a
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2018-03-12 00:44:37 -0300

    Update calendar.js
    corrention js

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -6,6 +6,7 @@ $(document).ready(function()
             $monthYear =$('.month_year'),
             $prevMonth =$('.monthButtonPrev'),
             $nextMonth =$('.monthButtonNext'),
+
             showWeekDays = false;
         
 
@@ -18,19 +19,19 @@ $(document).ready(function()
         var monthNames = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
         //var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday", "Saturday"];
         var dayNames = ["Sun","Mon","Tue","Wed","Thr","Fri", "Sat"];
-        var dayPerMonth = ["31", february(),"31","30","31","30","31","31","30","31","30","31"];
+        var dayPerMonth = ["31",february() ,"31","30","31","30","31","31","30","31","30","31"];
         
+
         function february(){
             //Determing if February (28,or 29)
-            if (current_month == 1){
-                if ( (current_year%100!=0) && (current_year%4==0) || (current_year%400==0)){
-                    return "29";
-                }else{
-                    return "28";
-                }
-            }
+            if ( (current_year%100!=0) && (current_year%4==0) || (current_year%400==0)){
+                return "29";
+           }else{
+                return  "28";
+           }
         }
         function displayCalendar(){
+            
             $monthYear.append( monthNames[current_month] + "<br> <span >"+current_year+"</span>");            
 
             if (!showWeekDays){
@@ -41,11 +42,11 @@ $(document).ready(function()
             }
             
             var prevDate = new Date((current_month+1) +' 1 ,'+current_year);
-        
+            console.log(prevDate);
             for (previous_day = prevDate.getDay(); previous_day > 0; previous_day -- ){
                 if (current_month == 0 ){
                     $dayList.append("<li class = 'day_prev'>"+(dayPerMonth[11] - previous_day + 1)+"</li>");
-                }else{
+                }else{        
                     $dayList.append("<li class = 'day_prev'>"+(dayPerMonth[prevDate.getMonth()-1] - previous_day + 1)+"</li>");
                 }
             }
@@ -58,6 +59,7 @@ $(document).ready(function()
                     $dayList.append("<li>"+day+"</li>");
                 }
             }
+
             var next_day = 1;
             if (($('li').length - 7) > 35){
                 while ($('li').length-7 < 42){
@@ -109,4 +111,3 @@ $(document).ready(function()
     };
     dayList();    
 });
-
